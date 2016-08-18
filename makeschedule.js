7b86adf6fd49c357c0b969037f640ebebaed3b47
Julian <julian.pinzer+development@gmail.com>
2016-08-18 02:26:56 -0400

    First Commit
    It downloads the schedule, however it puts the entire week's schedule
    onto the Saturday of the week. Annoying.

--- makeschedule.js
+++ makeschedule.js
@@ -6,80 +6,97 @@ window.randomCSSColor = function() {
 }
 document.addEventListener('load', function(){
 	window.makeSchedule = function(){
-	var termend = new Date(2014, 11, 19,0,0,0,0); //hack, definitely do this better later
-	var arr = document.getElementsByClassName("ddlabel");
-	// more hacks - trying to get this out fast
-	var monday = new Date(document.getElementsByClassName("fieldlargetext")[0].innerHTML.split("Week of ")[1]);
-	// better solution would be to use Date.now and then figure out when the weekdays are in proximity to the current date
+		console.log("Button Clicked");
+		var termend = new Date(2016, 10, 13,0,0,0,0); //hack, definitely do this better later
+		var arr = document.getElementsByTagName('tbody')[7];//hardcoded to find the 7th table on the page, should be the schedule
+		console.log(arr);
+		// more hacks - trying to get this out fast
+		var monday = new Date(document.getElementsByClassName("fieldlargetext")[0].innerHTML.split("Week of ")[1]);
+		console.log(monday)
+		// better solution would be to use Date.now and then figure out when the weekdays are in proximity to the current date
 
-	// takes a Date object and a string that's not formatted on bannerweb and returns the date with the time tacked on
-	var addHoursAndMinutes = function(date, time) {
-		var timestrings = time.split(' '); // gives you an array of 2, time and am or pm
-		var hourandminute = timestrings[0].split(":");
-		var hour = parseFloat(hourandminute[0]);
-		if (timestrings[1] === "pm" && hour !== 12) hour += 12;
-		var minute = parseFloat(hourandminute[1]);
-		date.setHours(hour);
-		date.setMinutes(minute);
-		return date; // technically since it's a reference we don't need to return but it's easier for me to read like this
-	}
-
-	if (arr.length <= 0) return;
-	window.cal = ics(); // build a cal
-	for (var i = 0; i < arr.length; i++) {
-		// see if we have a table element with a link inside it (link text has course info on it)
-		if (arr[i].children[0] && arr[i].children[0].tagName.toLowerCase() === "a") {
-			var parentChildren = Array.prototype.slice.call(arr[i].parentElement.children);
-			var dayOfWeek = parentChildren.indexOf(arr[i]) -1; // 0 is Monday, 6 is Sunday, bannerweb is weird
-			// now convert a bannwerb date to a w3c spec'd date
-			if (dayOfWeek === 6) dayOfWeek = 0; // sunday becomes last
-			else dayOfWeek+= 1;
-			// all the other days get a little bigger since Sunday dipped past monday
-
-			// build a Date object based off of monday
-			var workingDate = new Date(monday.getTime()); // getTime() returns a date expressed as a huge number
-			// done this way because Date's have no native clone method, but this gets the job done
-			var workingDay = workingDate.getDay();
-			var distance = dayOfWeek - workingDay;
-			//var distance = workingDay - dayOfWeek;
-			// essentially we scraped Monday off the page, then tack on whatever day the DOM says a particular class is on 
-			workingDate.setDate(workingDate.getDate() + distance); 
-
-			var calItem = arr[i].children[0].innerHTML.split("<br>");
-
-			// yank crap out of the DOM
-			var course = calItem[0];
-			var description = calItem[1];
-			var location = calItem[3];
-			var times = calItem[2].split("-"); // contains two strings with start and end times 
-
-
-			// "clone" two new Date() objects
-			var start = addHoursAndMinutes(new Date(workingDate.getTime()), times[0]);
-			var end = addHoursAndMinutes(new Date(workingDate.getTime()), times[1]);
-			
-			// take wpi course data and build an ICS object 
-			var rrule = {
-				freq: "WEEKLY",
-				until: termend,
-				interval: 1 // repeat every week... setting to 2 would do 2 weeks... etc
-			}
-			// now that we have everything in place, add it to cal
-			cal.addEvent(course, description, location, start, end, rrule);
+		// takes a Date object and a string that's not formatted on bannerweb and returns the date with the time tacked on
+		var addHoursAndMinutes = function(date, time) {
+			var timestrings = time.split(' '); // gives you an array of 2, time and am or pm
+			var hourandminute = timestrings[0].split(":");
+			var hour = parseFloat(hourandminute[0]);
+			if (timestrings[1] === "pm" && hour !== 12) hour += 12;
+			var minute = parseFloat(hourandminute[1]);
+			date.setHours(hour);
+			date.setMinutes(minute);
+			return date; // technically since it's a reference we don't need to return but it's easier for me to read like this
 		}
-	}	
+
+		if (arr.length <= 0) return;
+		window.cal = ics(); // build a cal
+		for (var i = 1; i < arr.rows.length; i++) {
+			console.log("arr len "+ arr.rows.length);
+			console.log("Looping array, " + i + ".");
+			// see if we have a table element with a link inside it (link text has course info on it)
+			console.log(arr.children[1]);
+			console.log(arr.children[1].children[2]);
+
+			if (arr.children[i]){
+				for(var j = 1; j < arr.rows.length; j++) {
+					if(typeof arr.children[i].children[j] !== "undefined")
+					if(typeof arr.children[i].children[j].children[0] !== "undefined" && 
+					   arr.children[i].children[j].children[0].tagName.toLowerCase() === "a"){
+						console.log(arr.children[i].children[j].children[0]);
+				var parentChildren = Array.prototype.slice.call(arr.parentElement.children);
+				var dayOfWeek = parentChildren.indexOf(arr[i]) -1; // 0 is Monday, 6 is Sunday, bannerweb is weird
+				// now convert a bannwerb date to a w3c spec'd date
+				if (dayOfWeek === 6) dayOfWeek = 0; // sunday becomes last
+				else dayOfWeek+= 1;
+				// all the other days get a little bigger since Sunday dipped past monday
+
+				// build a Date object based off of monday
+				var workingDate = new Date(monday.getTime()); // getTime() returns a date expressed as a huge number
+				// done this way because Date's have no native clone method, but this gets the job done
+				var workingDay = workingDate.getDay();
+				var distance = dayOfWeek - workingDay;
+				//var distance = workingDay - dayOfWeek;
+				// essentially we scraped Monday off the page, then tack on whatever day the DOM says a particular class is on 
+				workingDate.setDate(workingDate.getDate() + distance); 
+
+				var calItem = arr.children[i].children[j].children[0].innerHTML.split("<br>");
+
+				// yank crap out of the DOM
+				var course = calItem[0];
+				var description = calItem[1];
+				var location = calItem[3];
+				var times = calItem[2].split("-"); // contains two strings with start and end times 
+				console.log(course);
+
+				// "clone" two new Date() objects
+				var start = addHoursAndMinutes(new Date(workingDate.getTime()), times[0]);
+				var end = addHoursAndMinutes(new Date(workingDate.getTime()), times[1]);
+				
+				// take wpi course data and build an ICS object 
+				var rrule = {
+					freq: "WEEKLY",
+					until: termend,
+					interval: 1 // repeat every week... setting to 2 would do 2 weeks... etc
+				}
+				// now that we have everything in place, add it to cal
+				cal.addEvent(course, description, location, start, end, rrule);
+					}
+				}
+
+			}
+		}		
 	cal.download();
-}
-var btn = document.createElement("input");
-btn.id = "insertedbtn";
-if (document.getElementById("insertedbtn")!== null || document.URL.indexOf("pls/prod/bwskfshd") === -1) return; // super lazy
-btn.type = "button";
-btn.value = "Download Schedule!!!";
-btn.onclick = function(){window.makeSchedule()};
-btn.style.width = "100%";
-btn.style.height = "200px";
-document.body.children[0].insertBefore(btn);
-btn.style.fontSize = "32pt";
-var anon = function() { btn.style.backgroundColor = window.randomCSSColor();window.setTimeout(anon, 500);}
-anon();
+	}
+	console.log("Hello, WPI")
+	var btn = document.createElement("input");
+	btn.id = "insertedbtn";
+	if (document.getElementById("insertedbtn")!== null || document.URL.indexOf("pls/prod/bwskfshd") === -1) return; // super lazy
+	btn.type = "button";
+	btn.value = "Download Schedule!!!";
+	btn.onclick = function(){window.makeSchedule()};
+	btn.style.width = "100%";
+	btn.style.height = "200px";
+	document.body.insertBefore(btn, document.body.children[0]);
+	btn.style.fontSize = "32pt";
+	//var anon = function() { btn.style.backgroundColor = window.randomCSSColor();window.setTimeout(anon, 500);}
+	anon();
 } , true);
